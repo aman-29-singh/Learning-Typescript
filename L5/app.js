38be1f578f46449aff04a11c78cf9e538906ea90
aman-29-singh <singhaman41004@gmail.com>
2025-11-27 19:00:36 +0530

    L5 added learning unknown and never Types

---
+++ L5/app.js
@@ -0,0 +1,30 @@
+var userInput;
+var userName;
+var userInfo;
+userInput = 10; //iske andar kuch bhi dataType ka value rakh skte hai like any
+//iske andar kuch bhi dataType ka value rakh skte hai like any to unknown ka fayda hii kya hai 
+//toh any hii rakh lo userInput mein userInput : unknown
+userName = "aman";
+//userName = userInput; //toh error aayega because of unknown dataType userInput ka isliye error aayega
+userName = userInfo; //it is valid error nhi aayega because userInfo is any dataType
+//so it is major difference between unknown and any
+//toh any matlab kuch bhi kaun sa bhi dataType daalo aur unknown matlab tum kuch bhi nahi daale ye bhi ho skta hai
+//so iske liye userName = userInput; we have to give condition
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+//Now function mein humne return Type dekha tha toh function ka return type ek never bhi hota hai
+//Now we will see the never return Type
+function generateError(message, code) {
+    throw { message: message, statusCode: code };
+    //toh ye toh patta hii nhi chal rha ki ye function kuch retturn kr rha hai ya message print kr rha h
+    //so iss function ka return type humein pata nhi chal rha hai so issi ka return type never hota hai
+    //so agar ye function kuch return nhi karta sirf message print krta to return type void likhte
+    //but iss function mein throw krke aaraha hai means isko void nhi bol sakte aur nahi koi normal datatype jaise intt etc
+    //toh iska return type never hota hai toh ye kuch return krta hii nhi
+    //toh iss function mein return use nhi kar sakte hai because ismein throw use hua hai
+    //toh jaise hii function mein throw ye line executte hoga toh poora function kill ho jayegga 
+    //toh throw line jaise hii execute hoga poora fuiunction kill ho jayega matlab function terminate ho jayega yahin se niche jayega hii nhi  
+}
+var res = generateError("Internal server error", 500);
+console.log(res);
